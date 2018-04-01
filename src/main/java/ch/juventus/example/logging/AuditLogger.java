354078a639508f31f23bac688581f4bedb2341b4
Sebastian Haehnel <sebastian.haehnel@gmail.com>
2018-04-01 12:52:04 +0200

    fixing logging the broken return value with spring data

--- src/main/java/ch/juventus/example/logging/AuditLogger.java
+++ src/main/java/ch/juventus/example/logging/AuditLogger.java
@@ -5,7 +5,6 @@ import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Aspect @Configuration
 public class AuditLogger {
@@ -22,10 +21,10 @@ public class AuditLogger {
         logger.info("Entering {} with parameters {}.", operation, args);
     }
 
-    @AfterReturning(pointcut = "auditLogMethods()", returning = "returnValue")
-    public void leaveMethodWithReturnvalue(JoinPoint joinPoint, Object returnValue) {
+    @AfterReturning(pointcut = "auditLogMethods()")
+    public void leaveMethodWithReturnvalue(JoinPoint joinPoint) {
         String operation = joinPoint.getSignature().getName();
-        logger.info("Leaving {} with return value {}.", operation, returnValue);
+        logger.info("Leaving {}.", operation);
     }
 
     @AfterThrowing(pointcut = "auditLogMethods()", throwing = "throwable")
